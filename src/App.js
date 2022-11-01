16db23b0670196418c946b3683342cd5bffc9a2d
DefiLord <gkozlowskibusiness@outlook.com>
2022-11-01 16:53:48 -0400

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./componets/Navigation";
+import Hero from "./componets/Hero";
+import Guides from "./componets/Guides";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Hero />
+      <Guides />
     </div>
   );
 }
---
+++ src/componets/Hero.jsx
@@ -0,0 +1,18 @@
+
+export default function Example() {
+  return (
+      
+<section class="pt-20 py-2 flex items-center h-full justify-center  bg-gradient-to-b from-black to-indigo-900">
+  <div class="mx-auto max-w-[30rem]">
+    <div class="text-center">
+        <br></br><br></br>
+      <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Introducing the future of Web3 Management</p>
+      <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
+      <p class="mt-10 text-lg leading-relaxed text-slate-400">MOT helps unify your web3 community, organization and 
+      brand giving you a platform manage your componets in a more organized fashion creating organizations with real identities.</p>
+    </div>
+  </div>
+</section>
+ 
+  )
+}
