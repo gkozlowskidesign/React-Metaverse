7e909a270ea4bb9b6adca2f8b95e8de9c14d13a7
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 13:22:12 -0500

    Update Hero.jsx

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -10,12 +10,11 @@ export default function Example() {
         <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
         <p class="mt-10 text-lg leading-relaxed text-slate-400">
         <br></br><hr></hr><br></br>
-          MOT helps unify your web3 community or organization by 
-        enabling better brand identity. We are doing this through using key componets of Scrum, and Agile Project Management which 
-        major technology companies use today to be successful and productive. We aim to develop a PaaS 
-        otherwise known as a Platform as a Service to help you learn about both Scrum and Agile so you can manage your brand 
-        web3 business componets in a more organized fashion giving your organizations the mobility to create something unquie and
-        identifiable.
+        MOT helps unify your web3 community or organization by enabling better brand identity. 
+        We are doing this through using key components of Scrum, and Agile Project Management which major technology 
+        companies use today to be successful and productive. We aim to develop a PaaS otherwise known as a Platform as a 
+        Service to help you learn about both Scrum and Agile so you can manage your brand web3 business components in 
+        a more organized fashion giving your organizations the mobility to create something unique and identifiable.
         <br></br><br></br><hr></hr> <br></br><br></br></p>
       </div>
     </div>
