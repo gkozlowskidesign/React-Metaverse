95c18fe3a6b5c6734dfe96a283ef2f483028c2d1
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 01:40:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import NavBar from "./componets/Navigation";
 import Hero from "./componets/Hero";
 import Guides from "./componets/Guides";
+import Blog from "./componets/Blog";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
+    <div className="App">
       <NavBar />
-      <Hero />
-      <Guides />
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Hero/>
+          </Route>
+          <Route  path="/guide">
+          <Hero/>
+          <Guides />
+          </Route>
+          <Route  path="/blog">
+          <Hero />
+          <Blog/>
+        </Route>
+      </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
