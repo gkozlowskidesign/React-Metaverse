69fc3e04eac2efcee7224a6303e90754487ac43f
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 12:24:05 -0500

    Update Navigation.jsx

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -5,7 +5,7 @@ import {
   Bars3Icon,
   XMarkIcon,
   CubeIcon,
-  Academic-CapIcon,
+  AcademicIcon,
 
 } from '@heroicons/react/24/outline'
 import {GoProject } from "react-icons/go";
@@ -20,7 +20,7 @@ const solutions = [
   {
     name: 'Learning',
     href: '/blog',
-    icon: Academic-CapIcon,
+    icon: AcademicIcon,
   },
   { name: 'Documentation', 
     href: 'https://github.com/orgs/Metaverse-Of-Things/projects', 
