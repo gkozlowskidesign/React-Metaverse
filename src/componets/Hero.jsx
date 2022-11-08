2b3d70617f528c24551eb638af9b75ff68e16ff3
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 01:42:56 -0500

    Update Hero.jsx

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -1,18 +1,25 @@
 
 export default function Example() {
-  return (
-      
-<section class="pt-20 py-2 flex items-center h-full justify-center  bg-gradient-to-b from-black to-indigo-900">
-  <div class="mx-auto max-w-[30rem]">
-    <div class="text-center">
-        <br></br><br></br>
-      <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Introducing the future of Web3 Management</p>
-      <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
-      <p class="mt-10 text-lg leading-relaxed text-slate-400">MOT helps unify your web3 community, organization and 
-      brand giving you a platform manage your componets in a more organized fashion creating organizations with real identities.</p>
+    return (
+        
+  <section class="pt-20 py-2 flex items-center h-full justify-center  bg-gradient-to-b from-indigo-900 to-black">
+    <div class="mx-auto max-w-[30rem]">
+      <div class="text-center">
+          <br></br><br></br>
+        <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Introducing the future of Web3 Management</p>
+        <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
+        <p class="mt-10 text-lg leading-relaxed text-slate-400">
+        <br></br><hr></hr><br></br>
+          MOT helps unify your web3 community or organization by 
+        enabling better brand identity. We are doing this through using key componets of Scrum, and Agile Project Management which 
+        major technology companies use today to be successful and productive. We aim to develop a PaaS 
+        otherwise known as a Platform as a Service to help you learn about both Scrum and Agile so you can manage your brand 
+        web3 business componets in a more organized fashion giving your organizations the mobility to create something unquie and
+        identifiable.
+        <br></br><br></br><hr></hr> <br></br><br></br></p>
+      </div>
     </div>
-  </div>
-</section>
- 
-  )
+  </section>
+   
+    )
 }
