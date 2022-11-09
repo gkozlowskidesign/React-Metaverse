30ed573962535a3b1a87c580b662b317b71d922d
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 22:32:19 -0500

    Update Blog.jsx

--- src/componets/Blog.jsx
+++ src/componets/Blog.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Blog = () => {
   return (
-<section class="mb-6 bg-black text-gray-600 body-font">
+<section class="mb-4 bg-black text-gray-600 body-font">
     
-    <section class="pt-4 pb-4 py-2 flex items-center h-full justify-center  bg-gradient-to-b from-indigo-900 to-black">
+    <section class="pt-4 pb-10 py-2 flex items-center h-full justify-center  bg-gradient-to-b from-indigo-900 to-black">
     <div class="mx-auto max-w-[30rem]">
       <div class="text-center">
           <br></br><br></br>
         <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Introducing the future of Web3 Management</p>
         <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
-        <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Web3 Foundations</p>
+        <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Learning</p>
   
     </div>
     </div>
