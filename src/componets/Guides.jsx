f66745daafa5e2add227d9b88325d85fb8274c56
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 22:37:59 -0500

    Update Guides.jsx

--- src/componets/Guides.jsx
+++ src/componets/Guides.jsx
@@ -22,11 +22,11 @@ const About = () => {
     
         <div class="max-w-screen-lg p-8 mx-auto flex flex-col justify-center w-full h-full gap-10">
     
-      <div class=" bg-gradient-to-b from-gray-900 to-gray-400  p-6 shadow-lg rounded-2xl flex justify-between items-center">
+      <div class=" bg-gradient-to-b from-gray-600 to-gray-200  p-6 shadow-lg rounded-2xl flex justify-between items-center">
    
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
-                        <SiGithub className="shadow m-2 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-gray-700 text-white" aria-hidden="true" />
+                        <SiGithub className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-gray-700 text-white" aria-hidden="true" />
                         </div>
                     <div>
                         <h1 class="ml-5 mb-3 text-xl font-medium text-white">GitHub</h1>
@@ -39,7 +39,7 @@ const About = () => {
         <div class=" bg-gradient-to-b from-purple-400 to-purple-600  p-6 shadow-lg rounded-2xl  flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
-                        <SiMedium className="shadow m-2 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-black text-white" aria-hidden="true" />
+                        <SiMedium className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-black text-white" aria-hidden="true" />
                         </div>
                     <div>
                         <h1 class="ml-5 mb-3 text-xl font-medium text-white">Medium</h1>
@@ -52,7 +52,7 @@ const About = () => {
         <div class=" bg-gradient-to-b from-indigo-500 to-indigo-800  p-6 shadow-lg rounded-2xl flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
-                        <SiDiscord className="shadow m-2 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-indigo-700 text-white" aria-hidden="true" />
+                        <SiDiscord className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-indigo-700 text-white" aria-hidden="true" />
                         </div>
                     <div>
                         <h1 class="ml-5 mb-3 text-xl font-medium text-white">Discord</h1>
@@ -65,7 +65,7 @@ const About = () => {
             <div class=" bg-gradient-to-b from-blue-800 to-blue-500  p-6 shadow-lg rounded-2xl flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
-                        <SiLinkedin className="shadow m-2 border-2 sm:w-12 sm:h-12  w-14 h-14 p-2 rounded text-white" aria-hidden="true" />
+                        <SiLinkedin className="shadow m-4 border-2 sm:w-12 sm:h-12  w-14 h-14 p-2 rounded text-white" aria-hidden="true" />
                         </div>
                     <div>
                         <h1 class="ml-5 mb-3 text-xl font-medium text-white">LinkedIn</h1>
@@ -79,7 +79,7 @@ const About = () => {
             <div class=" bg-gradient-to-b from-blue-400 to-blue-100  p-6 shadow-lg rounded-2xl flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
-                        <SiTwitter className="shadow m-2 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-blue-400 text-white" aria-hidden="true" />
+                        <SiTwitter className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-blue-400 text-white" aria-hidden="true" />
                         </div>
                     <div>
                         <h1 class="ml-5 mb-3 text-xl font-medium text-white">Twitter</h1>
@@ -93,7 +93,7 @@ const About = () => {
             <div class=" bg-gradient-to-b from-orange-600 to-yellow-400  p-6 shadow-lg rounded-2xl  flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
-                        <SiInstagram className="shadow m-2 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-red-500 text-white" aria-hidden="true" />
+                        <SiInstagram className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-red-500 text-white" aria-hidden="true" />
                         </div>
                     <div>
                         <h1 class="ml-5 mb-3 text-xl font-medium text-white">Instagram</h1>
