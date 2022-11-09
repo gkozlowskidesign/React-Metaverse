b7c023ca6081fd2b22781a3e0b97fb4648c9ce59
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 21:44:55 -0500

    Create Documentation.jsx

---
+++ src/componets/Documentation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+<section class="mb-20 bg-black text-gray-600 body-font">
+ 
+    <div class="flex flex-wrap">
+            <iframe className='mb-40' src="https://metaverse-of-things.gitbook.io/introduction/" name="iframe_a" scrolling="no" height="2800rem" width="100%" title="Iframe Example"></iframe>
+    </div>
+
+</section>
+  )
+}
+
+export default Documentation
