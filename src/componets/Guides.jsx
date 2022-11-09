60df104dd4dba2c7c21bd68c74f47dbe51fb1a97
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-08 22:33:10 -0500

    Update Guides.jsx

--- src/componets/Guides.jsx
+++ src/componets/Guides.jsx
@@ -7,13 +7,15 @@ const About = () => {
   return (
     <div name="about" className='w-ful h-100 py-2 bg-gradient-to-b from-indigo-900 to-black'>
     
-        <section class="pt-4 pb-10 py-2 flex items-center h-full justify-center">
+        <section class="pt-4 pb-14 py-2 flex items-center h-full justify-center">
     <div class="mx-auto max-w-[30rem]">
       <div class="text-center">
           <br></br><br></br>
         <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Introducing the future of Web3 Management</p>
         <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
-      </div>
+            <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Networking</p>
+  
+    </div>
     </div>
   </section>
     
