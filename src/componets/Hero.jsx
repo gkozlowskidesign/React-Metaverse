97ee909a5d2c0e89d71899ff8bb3753f2142744b
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-09 17:54:10 -0500

    Update Hero.jsx

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -2,24 +2,23 @@
 export default function Example() {
     return (
         
-  <section class="pt-20 pb-10 py-2 flex items-center h-full justify-center  bg-gradient-to-b from-indigo-900 to-black">
-    <div class="mx-auto max-w-[30rem]">
+  <section class="pt-5 py-10 flex items-center h-full justify-center bg-gradient-to-b from-indigo-900 to-black">
+    <div class="mx-auto max-w-[27.5rem]">
       <div class="text-center">
-          <br></br><br></br>
-        <p class="mb-8 text-lg font-medium leading-8 text-indigo-600/95">Introducing the future of Web3 Management</p>
+        <p class="mb-8 text-lg font-medium leading-8 text-white">Introducing the future of Web3 Management</p>
         <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
-        <p class="mb-8 text-lg font-medium leading-8 text-white">Home</p>
-        <p class="mt-10 text-lg leading-relaxed text-slate-400">
+        <p class="mb-2 text-lg font-medium leading-8 text-white">Home</p>
+        <p class="m-10 mt-3 text-lg leading-relaxed text-slate-400">
         <br></br><hr></hr><br></br>
-        MOT helps unify your web3 community or organization by enabling better brand identity. 
-        We are doing this through using key components of Scrum, and Agile Project Management which major technology 
-        companies use today to be successful and productive. We aim to develop a PaaS otherwise known as a Platform as a 
-        Service to help you learn about both Scrum and Agile so you can manage your brand web3 business components in 
-        a more organized fashion giving your organizations the mobility to create something unique and identifiable.
-        <br></br><br></br><hr></hr> <br></br><br></br></p>
+          MOT helps unify your web3 community or organization by 
+        enabling better brand identity. We are doing this through using key componets of Scrum, and Agile Project Management which 
+        major technology companies use today to be successful and productive. We aim to develop a PaaS 
+        otherwise known as a Platform as a Service to help you learn about both Scrum and Agile so you can manage your brand 
+        web3 business componets in a more organized fashion giving your organizations the mobility to create something unquie and
+        identifiable.
+        <br></br><br></br><hr></hr></p>
       </div>
     </div>
   </section>
-   
     )
 }
