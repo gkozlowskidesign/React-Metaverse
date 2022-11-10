32df9018ba38f6e8632e479cb1744402e6efd75a
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-10 16:33:55 -0500

    Update Projects.jsx

--- src/componets/Projects.jsx
+++ src/componets/Projects.jsx
@@ -111,13 +111,6 @@ export default function Example() {
                    Projects
                    <span className="text-indigo-200" aria-hidden="true"></span>
                  </a>
-                 <a
-                   href="https://metaverseofthings.medium.com/"
-                   className="inline-block rounded-lg bg-indigo-900 px-5 py-3.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-400 hover:ring-indigo-700"
-                 >
-                   Tools
-                   <span className="text-indigo-200" aria-hidden="true"></span>
-                 </a>
                </div>
 
         </div>
