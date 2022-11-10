f878853748f15bdb4f816ba04c2c886e638ac361
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-10 16:38:41 -0500

    Update Hero.jsx

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -5,8 +5,9 @@ export default function Example() {
   <section class="pt-15 py-10 flex items-center h-full justify-center bg-gradient-to-b from-indigo-900 to-black">
     <div class="mx-auto max-w-[27.5rem]">
       <div class="text-center">
-        <p class="mb-8 text-lg font-medium leading-8 text-white">Introducing the future of Web3 Management</p>
+      <p class="mb-8 text-lg font-medium leading-8 text-white">Infrastructure That Supports The World</p>
         <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
+        <p class="mb-2 text-lg font-medium leading-8 text-white">MoTeX</p>
         <p class="mb-2 text-lg font-medium leading-8 text-white">Home</p>
         <p class="m-10 mt-3 text-lg leading-relaxed text-slate-400">
         <br></br><hr></hr><br></br>
