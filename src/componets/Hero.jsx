af402422a1ca6b5d0b98c7db6f3e63c24f272e56
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-10 16:42:17 -0500

    Update Hero.jsx

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -8,7 +8,6 @@ export default function Example() {
       <p class="mb-8 text-lg font-medium leading-8 text-white">Infrastructure That Supports The World</p>
         <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
         <p class="mb-2 text-lg font-medium leading-8 text-white">MoTeX</p>
-        <p class="mb-2 text-lg font-medium leading-8 text-white">Home</p>
         <p class="m-10 mt-3 text-lg leading-relaxed text-slate-400">
         <br></br><hr></hr><br></br>
           MOT helps unify your web3 community or organization by 
