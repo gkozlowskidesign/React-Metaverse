c65868a87e6747749117601cb87763e37dc2dc1b
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-10 16:41:01 -0500

    Update Guides.jsx

--- src/componets/Guides.jsx
+++ src/componets/Guides.jsx
@@ -12,8 +12,7 @@ const About = () => {
      <div class="text-center">
       <p class="mb-8 text-lg font-medium leading-8 text-white">Infrastructure That Supports The World</p>
         <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
-        <p class="mb-2 text-lg font-medium leading-8 text-white">MoTeX</p>
-        <p class="mb-5 text-lg font-medium leading-8 text-white">Networking</p>
+        <p class="mb-5 text-lg font-medium leading-8 text-white">Community</p>
       </div>
     </div>
   </section>
