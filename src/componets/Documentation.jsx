c02c66c627decb949f1521bcde41055682fd11e9
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-10 17:35:05 -0500

    Update Documentation.jsx

--- src/componets/Documentation.jsx
+++ src/componets/Documentation.jsx
@@ -4,16 +4,19 @@ const Documentation = () => {
   return (
 <section class="mb-6 bg-black text-gray-600 body-font">
     
-  <section class="pt-4 pb-20 py-2 flex items-center h-full justify-center  bg-gradient-to-b from-indigo-900 to-black">
-    <div class="mx-auto max-w-[30rem]">
-      <div class="text-center">
-        <p class="mb-8 text-lg font-medium leading-8 text-white">Infrastructure That Supports The World</p>
-        <h1 class="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br><span className='text-indigo-600'>of Things</span></h1>
-        <p class="mb-8 text-lg font-medium leading-8 text-white">Documentation</p>
- 
+<section className='pt-10 py-80 flex items-center h-100 justify-center bg-gradient-to-b from-indigo-900 to-black'>
+    <div className="mx-auto max-w-[27.5rem]">
+      <div className="text-center">
+            <div className="pt-2  flex items-center justify-center ">
+                <img className="h-30" src={logo} alt="MoTeX"/> 
+            </div>     
+            <p className="mb-2 text-lg font-medium leading-8 text-white">MoTeX Documentation</p>
+            <h1 className="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br>
+            <span className='text-indigo-600'>of Things</span></h1>
+            <p className="mb-2 text-lg font-medium leading-8 text-white">Future Infrastructure</p>      
+      </div>
     </div>
-    </div>
-  </section>
+</section>
  
     <div class="flex flex-wrap">
             <iframe className='mb-40' src="https://metaverse-of-things.gitbook.io/docs/" name="iframe_a" scrolling="no" height="2800rem" width="100%" title="Iframe Example"></iframe>
