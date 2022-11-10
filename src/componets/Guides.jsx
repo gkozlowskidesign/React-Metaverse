057e75eba2a9887e10172ca50b419c96c0095c13
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-10 17:32:08 -0500

    Update Guides.jsx

--- src/componets/Guides.jsx
+++ src/componets/Guides.jsx
@@ -6,47 +6,39 @@ const About = () => {
 
 
   return (
-    <div name="about" className='w-ful h-100 py-2 bg-gradient-to-b from-indigo-900 to-black'>
     
-      <div className="bg-black">
+<div className="bg-black">
 
 <section className='pt-10 py-80 flex items-center h-100 justify-center bg-gradient-to-b from-indigo-900 to-black'>
     <div className="mx-auto max-w-[27.5rem]">
       <div className="text-center">
-      <div className="pt-2  flex items-center justify-center ">
-            <img
-            className="h-30"
-            src={logo}                    
-            alt="MoTeX"
-            /> 
+            <div className="pt-2  flex items-center justify-center ">
+                <img className="h-30" src={logo} alt="MoTeX"/> 
             </div>     
             <p className="mb-2 text-lg font-medium leading-8 text-white">MoTeX Community</p>
-        <h1 className="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br>
-        <span className='text-indigo-600'>of Things</span></h1>
- <p className="mb-10 text-lg font-medium leading-8 text-white">Future Infrastructure</p>      
-</div>
-</div>
+            <h1 className="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br>
+            <span className='text-indigo-600'>of Things</span></h1>
+            <p className="mb-10 text-lg font-medium leading-8 text-white">Future Infrastructure</p>      
+      </div>
+    </div>
 </section>
 
     
     
-        <div class="max-w-screen-lg p-8 mx-auto flex flex-col justify-center w-full h-full gap-10">
-    
+   <div class="max-w-screen-lg p-8 mx-auto flex flex-col justify-center w-full h-full gap-10">
       <div class=" bg-gradient-to-b from-gray-600 to-gray-200  p-6 shadow-lg rounded-2xl flex justify-between items-center">
-   
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
                         <SiGithub className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-gray-700 text-white" aria-hidden="true" />
-                        </div>
+                   </div>
                     <div>
                         <h1 class="ml-5 mb-3 text-xl font-medium text-white">GitHub</h1>
                         <p class="ml-5 hidden md:block text-black">Be first to join our developer community.</p>
                     </div>
                 </div>
                 <a href="https://github.com/Metaverse-Of-Things"><button class="ml-4 bg-gradient-to-b from-black to-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-4 py-2">Go!</button></a>
-            </div>
-
-        <div class=" bg-gradient-to-b from-purple-400 to-purple-600  p-6 shadow-lg rounded-2xl  flex justify-between items-center">
+      </div>
+      <div class=" bg-gradient-to-b from-purple-400 to-purple-600  p-6 shadow-lg rounded-2xl  flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
                         <SiMedium className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-black text-white" aria-hidden="true" />
@@ -57,9 +49,8 @@ const About = () => {
                     </div>
                 </div>
                 <a href="https://metaverseofthings.medium.com/"><button class="ml-4 bg-gradient-to-b from-black to-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-4 py-2">Go!</button></a>
-            </div>
-
-        <div class=" bg-gradient-to-b from-indigo-500 to-indigo-800  p-6 shadow-lg rounded-2xl flex justify-between items-center">
+       </div>
+       <div class=" bg-gradient-to-b from-indigo-500 to-indigo-800  p-6 shadow-lg rounded-2xl flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
                         <SiDiscord className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-indigo-700 text-white" aria-hidden="true" />
@@ -70,9 +61,8 @@ const About = () => {
                     </div>
                 </div>
                 <a href="https://discord.gg/BPqsEMdjDW"><button class="ml-4 bg-gradient-to-b from-black to-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-4 py-2">Go!</button></a>
-            </div>
-
-            <div class=" bg-gradient-to-b from-blue-800 to-blue-500  p-6 shadow-lg rounded-2xl flex justify-between items-center">
+        </div>
+        <div class=" bg-gradient-to-b from-blue-800 to-blue-500  p-6 shadow-lg rounded-2xl flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
                         <SiLinkedin className="shadow m-4 border-2 sm:w-12 sm:h-12  w-14 h-14 p-2 rounded text-white" aria-hidden="true" />
@@ -83,10 +73,8 @@ const About = () => {
                     </div>
                 </div>
                 <a href="https://www.linkedin.com/company/metaverse-of-things/?viewAsMember=true"><button class="ml-4 bg-gradient-to-b from-black to-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-4 py-2">Go!</button></a>
-            </div>
-
-
-            <div class=" bg-gradient-to-b from-blue-400 to-blue-100  p-6 shadow-lg rounded-2xl flex justify-between items-center">
+         </div>
+         <div class=" bg-gradient-to-b from-blue-400 to-blue-100  p-6 shadow-lg rounded-2xl flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
                         <SiTwitter className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-blue-400 text-white" aria-hidden="true" />
@@ -97,10 +85,8 @@ const About = () => {
                     </div>
                 </div>
                 <a href="https://twitter.com/metaverseot"><button class="ml-4 bg-gradient-to-b from-black to-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-4 py-2">Go!</button></a>
-            </div>
-
-
-            <div class=" bg-gradient-to-b from-orange-600 to-yellow-400  p-6 shadow-lg rounded-2xl  flex justify-between items-center">
+         </div>
+         <div class=" bg-gradient-to-b from-orange-600 to-yellow-400  p-6 shadow-lg rounded-2xl  flex justify-between items-center">
                 <div class="flex">
                     <div class="-my-2 hidden md:block">
                         <SiInstagram className="shadow m-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-red-500 text-white" aria-hidden="true" />
@@ -111,14 +97,11 @@ const About = () => {
                     </div>
                 </div>
                 <a href="https://www.instagram.com/metaverseofthings/"><button class="ml-4 bg-gradient-to-b from-black to-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-4 py-2">Go!</button></a>
-            </div>
-            
+         </div>
             <br></br>
             <br></br>
-
-       
-        </div>
     </div>
+ </div>
   )
 }
 
