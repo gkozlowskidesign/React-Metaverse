ba17f68337d143bc4dc6ad018596bd0451ff5050
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-10 17:20:47 -0500

    Update Concepts.jsx

--- src/componets/Concepts.jsx
+++ src/componets/Concepts.jsx
@@ -76,7 +76,7 @@ const products = [
 
 export default function Example() {
   return (
-    <div className="bg-gradient-to-b from-indigo-900">
+    <div className="bg-indigo-900">
 
 <section className='pt-20 py-80 flex items-center h-100 justify-center bg-gradient-to-b from-indigo-900 to-black'>
     <div className="mx-auto max-w-[27.5rem]">
@@ -88,11 +88,11 @@ export default function Example() {
             alt="MoTeX"
             /> 
             </div>     
-            <p className="mb-2 text-lg font-medium leading-8 text-white">MoTeX</p>
+            <p className="mb-2 text-lg font-medium leading-8 text-white">MoTeX Education</p>
         <h1 className="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br>
         <span className='text-indigo-600'>of Things</span></h1>
-        <p className="mb-10 text-lg font-medium leading-8 text-white">Education</p>
-      </div>
+ <p className="mb-10 text-lg font-medium leading-8 text-white">Future Infrastructure</p>      
+</div>
         <div className='p-4'> 
                <div className="mt-8 flex gap-x-4 justify-center">
                   <a
