3c47bb3d97e7b3efe34e71ce10a6cd979596c9ac
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-13 20:49:59 -0500

    Update Guides.jsx

--- src/componets/Guides.jsx
+++ src/componets/Guides.jsx
@@ -6,7 +6,7 @@ const About = () => {
 
 
   return (
-    <div name="about" className='w-full h-100 py-2 bg-gradient-to-b from-indigo-600 to-black'>
+    <div name="about" className='w-full h-100 py-2 bg-gradient-to-b from-indigo-200 to-black'>
         
     <div class="max-w-screen-lg p-8 mx-auto flex flex-col justify-center w-full h-full gap-10">
     
