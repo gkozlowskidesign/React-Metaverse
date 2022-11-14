83353122ed8c940b65400ca44628dc0ced836eb9
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-13 22:22:12 -0500

    Update Guides.jsx

--- src/componets/Guides.jsx
+++ src/componets/Guides.jsx
@@ -9,8 +9,8 @@ const About = () => {
     <div>
             <div name="about" className='w-full h-100 bg-gradient-to-b from-indigo-900 to-indigo-400'>
         
-                    <div class="w-full h-full max-w-screen-lg p-8 mx-auto flex flex-col justify-center gap-10 ">
-                        <section className='pt-10 py-80 flex items-center h-100 justify-center'>
+             <div class="w-full h-full  max-w-screen-lg p-6 mx-auto flex flex-col justify-center gap-2 ">
+                        <section className='pt-10 py-40 flex items-center h-100 justify-center'>
                             <div className="mx-auto max-w-[27.5rem]">
                                 <div className="text-center">
                                     <div className="pt-2  flex items-center justify-center ">
@@ -19,7 +19,7 @@ const About = () => {
                                     <p className="mb-2 text-lg font-medium leading-8 text-white">MoTeX Education</p>
                                     <h1 className="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br>
                                     <span className='text-indigo-600'>of Things</span></h1>
-                                    <p className="mb-10 text-lg font-medium leading-8 text-white">Future Infrastructure</p>      
+                                    <p className="mb-2 text-lg font-medium leading-8 text-white">Future Infrastructure</p>      
                                 </div>
                             </div>
                         </section>
