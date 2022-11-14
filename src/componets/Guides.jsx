f639f52c5be123621ac94c0e8faebb3aa75fd2d4
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-13 20:06:40 -0500

    Update Guides.jsx

--- src/componets/Guides.jsx
+++ src/componets/Guides.jsx
@@ -83,11 +83,11 @@ const About = () => {
                         <SiMedium className="shadow mt-4 ml-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-black text-white" aria-hidden="true" />
                         </div>
                     <div>
-                        <h1 class="ml-5 mb-3 text-xl font-medium text-white">Follow Us On Medium</h1>
+                        <h1 class="ml-5 mb-3 text-xl font-medium text-white">Medium</h1>
                         <p class="ml-5 text-black">Follow us on Medium if you want to learn more about Web3 Tools and Blockchain Concepts.</p>
                     </div>
                 </div>
-                <a href="https://metaverseofthings.medium.com/"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow Us!</button></a>
+                <a href="https://metaverseofthings.medium.com/"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow</button></a>
             </div>
 
             <div class=" bg-gradient-to-b from-black to-indigo-700 ring-1 ring-indigo-800 p-3 shadow-lg rounded-2xl flex justify-between items-center">
@@ -96,11 +96,11 @@ const About = () => {
                         <SiDiscord className="shadow mt-4 ml-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-indigo-700 text-white" aria-hidden="true" />
                         </div>
                     <div>
-                        <h1 class="ml-5 mb-3 text-xl font-medium text-white">Join the Discord Community!</h1>
+                        <h1 class="ml-5 mb-3 text-xl font-medium text-white">Discord</h1>
                         <p class="ml-5 text-black">Be first to join our community, its hard to see the vision so our whitelist will be limited.</p>
                     </div>
                 </div>
-                <a href="https://discord.gg/BPqsEMdjDW"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow Us!</button></a>
+                <a href="https://discord.gg/BPqsEMdjDW"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow</button></a>
             </div>
 
             <div class=" bg-gradient-to-b from-black to-indigo-700 ring-1 ring-indigo-800 p-3 shadow-lg rounded-2xl flex justify-between items-center">
@@ -113,19 +113,7 @@ const About = () => {
                         <p class="ml-5 text-black">Follow us on LinkedIn for Blockchain Career Tips, Cryptocurrency Project Publications and More!</p>
                     </div>
                 </div>
-                <a href="https://www.linkedin.com/company/metaverse-of-things/?viewAsMember=true"><button class="ml-4 bg-indigo-900 ring-1 ring-indigo-500 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow Us!</button></a>
-            </div>
-
-            <div class=" bg-gradient-to-b from-black to-indigo-700 ring-1 ring-indigo-800 p-3 shadow-lg rounded-2xl flex justify-between items-center">
-                <div class="flex">
-                    <div class="-my-2 hidden md:block">
-                    <img src={Mirror} alt="Mirror.xyz" className="shadow mt-4 ml-4 border-2 sm:w-12 sm:h-12 w-40 h-14 rounded bg-blue-200 text-white" aria-hidden="true" />                        </div>
-                    <div>
-                        <h1 class="ml-5 mb-3 text-xl font-medium text-white">Subscribe and collect our NFTs!</h1>
-                        <p class="ml-5 text-black">Learn about Web3 Tools, Blockchain Concepts and Cryptocurrency Projects.</p>
-                    </div>
-                </div>
-                <a href="https://mirror.xyz/0x38034A81D48cBf8D4c4C757856d14492C848015b"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow Us!</button></a>
+                <a href="https://www.linkedin.com/company/metaverse-of-things/?viewAsMember=true"><button class="ml-4 bg-indigo-900 ring-1 ring-indigo-500 border-1 border-indigo-300 hover:opacity-75 text-white rounded-full px-6 py-2">Follow</button></a>
             </div>
 
             <div class=" bg-gradient-to-b from-black to-indigo-700 ring-1 ring-indigo-800 p-3 shadow-lg rounded-2xl flex justify-between items-center">
@@ -134,11 +122,11 @@ const About = () => {
                         <SiTwitter className="shadow mt-4 ml-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-blue-400 text-white" aria-hidden="true" />
                         </div>
                     <div>
-                        <h1 class="ml-5 mb-3 text-xl font-medium text-white">Follow Us On Twitter!</h1>
+                        <h1 class="ml-5 mb-3 text-xl font-medium text-white">Twitter</h1>
                         <p class="ml-5 text-black">Follow us on twitter and interact with us to learn more about the project!</p>
                     </div>
                 </div>
-                <a href="https://twitter.com/metaverseot"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow Us!</button></a>
+                <a href="https://twitter.com/metaverseot"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow</button></a>
             </div>
 
 
@@ -148,11 +136,11 @@ const About = () => {
                         <SiInstagram className="shadow mt-4 ml-4 border-2 sm:w-12 sm:h-12 w-14 h-14 p-2 rounded bg-red-500 text-white" aria-hidden="true" />
                         </div>
                     <div>
-                        <h1 class="ml-5  mb-3 text-xl font-medium text-white">Follow Us on Instagram!</h1>
+                        <h1 class="ml-5  mb-3 text-xl font-medium text-white">Instagram</h1>
                         <p class="ml-5 text-black">Follow Us on Instagram for Project Updates, Important Blockchain News and More!</p>
                     </div>
                 </div>
-                <a href="https://www.instagram.com/metaverseofthings/"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow Us!</button></a>
+                <a href="https://www.instagram.com/metaverseofthings/"><button class="ml-4 bg-indigo-900 border-2 hover:opacity-75 text-white rounded-full px-6 py-2">Follow</button></a>
             </div>
             
             <br></br>
