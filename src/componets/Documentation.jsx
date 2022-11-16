bb75a49d1ca4d70b3d9a5b94bba2cfb5911d6ffb
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-16 00:38:10 -0500

    Update Documentation.jsx

--- src/componets/Documentation.jsx
+++ src/componets/Documentation.jsx
@@ -44,7 +44,7 @@ const Documentation = () => {
             <iframe className='mb-40' src="https://metaverse-of-things.gitbook.io/introduction/" name="iframe_a" scrolling="no" height="2800rem" width="100%" title="Iframe Example"></iframe>
     </div>
 
-</div>
+
             
                 
          
