89a48ae13e5d2cf4cff2b72158daf4424ad2e612
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-19 19:36:19 -0500

    Update Home.jsx

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -12,10 +12,13 @@ return (
                 Development of our base-level IoT software has already begun and we will deliver value for everyone before moving to more centrialized projects.</p><br></br>
                 <p>Our team is made up of strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
             </div>
-            <div class="grid grid-cols-3 gap-4 mt-8">
+            <div class="grid grid-cols-2 gap-4 mt-8">
                 <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
                 <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/7629313/pexels-photo-7629313.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
                 <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/11138000/pexels-photo-11138000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
+            </div>
+            <div class="grid grid-cols-1 gap-4 mt-8">
+                 <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/7629313/pexels-photo-7629313.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
             </div>
         </div>
     </div>
