efc5ded445bc40ccf7b63fccbf50d38fd256aa35
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-20 23:30:55 -0500

    Update Home.jsx

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const Home = () => {
 return (
-<div>
+<div class="bg-black">
     <div class="bg-black">
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
@@ -12,18 +12,63 @@ return (
                 Development of our base-level IoT software has already begun and we will deliver value for everyone before moving to more centrialized projects.</p><br></br>
                 <p>Our team is made up of strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
             </div>
-            <div class="grid grid-cols-2 gap-4 mt-2">
-                 <img class="mt-4 w-6/6 rounded-lg" src="https://images.pexels.com/photos/1910230/pexels-photo-1910230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
-                 <img class="mt-4 w-6/6 rounded-lg" src="https://images.pexels.com/photos/7629313/pexels-photo-7629313.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
-            </div>
-            <div class="grid grid-cols-2 gap-4 mt-2">
+            <div class="grid grid-cols-3 gap-4 mt-8">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/2921137/pexels-photo-2921137.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
+
                 <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
                 <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/11138000/pexels-photo-11138000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
+            </div>
+   </div>
+</div>
+
+                <div class="mx-auto bg-black mt-40">
+        
+                <h2 class="mx-auto text-center text-3xl font-extrabold text-gray-900 dark:text-white mb-10">Web3 Launchpad for Blockchain Ideas</h2>
+
+    <div class="grid grid-cols-2 md:lg:xl:grid-cols-2 gap-4 group bg-black  rounded-xl">
+
+            <div class="mr-10 p-6 w-4/6 h-6/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  rounded-xl">
+                <span class="p-5 rounded-full bg-indigo-900 text-white shadow-lg shadow-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+                            </svg>
+                    </span>
+                <p class="text-md font-medium text-gray-400 mt-3">Cross Compatible</p>
+            </div>
+
+            <div class="ml-10 p-6 w-4/6 h-6/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group   rounded-xl">
+                <span class="p-5 rounded-full bg-indigo-900 text-white shadow-lg shadow-black">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z" />
+                </svg>
+                </span>
+                <p class="text-md font-medium text-gray-400 mt-3">Cloud Architecture</p>
+            </div>
+
+           
+            <div class="mt-20 mr-10 p-6 w-4/6 h-6/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  rounded-xl">
+            <span class="p-5 rounded-full bg-indigo-900 text-white shadow-lg shadow-black">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                </svg>
+            </span>
+                <p class="text-md font-medium text-gray-400 mt-3">Device Ready</p>
+            </div>
+
+            <div class="mt-20 ml-10 p-6 w-4/6 h-6/6 mx-auto bg-gray-900 flex flex-col items-center text-center group  rounded-xl">
+            <span class="p-5 rounded-full bg-indigo-900 text-white shadow-lg shadow-black">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+                </svg>
+            </span>
+                <p class="text-md font-medium text-gray-400 mt-3">Secured & Tested</p>
             </div>
         </div>
     </div>
 </div>
+
 )};
 export default Home
             
    
+
