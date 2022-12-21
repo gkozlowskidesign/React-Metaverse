c40cd572309ef275f596b94bc24791a06f13337f
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-20 23:55:58 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -32,11 +32,23 @@ const About = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Documentation</p>
+                <p class="text-xl font-medium text-gray-400 mt-3">Announcements</p>
                 <p class="mt-2 text-sm text-slate-500">Team BrainEdge education is a bunch of highly focused, energetic
                     set of people.</p>
             </div>
       </a>
+    
+     <a href="/learning">
+            <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-200 cursor-pointer rounded-xl">
+                <span class="p-5 rounded-full bg-indigo-400 text-white shadow-lg shadow-black"><svg
+                        xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="1.5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+                    </svg></span>
+                <p class="text-xl font-medium text-gray-400 mt-3">Education</p>
+                <p class="mt-2 text-sm text-slate-500">Yet another year ! Yet another jewel in our crown!</p>
+            </div>
+       </a>
     <a href="https://twitter.com/metaverseot">
             <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-200 cursor-pointer rounded-xl">
                 <span class="p-5 rounded-full bg-indigo-400 text-white shadow-lg shadow-black"><svg
@@ -65,18 +77,6 @@ const About = () => {
                     preparation.</p>
             </div>
            </a>
-
-      <a href="/learning">
-            <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-200 cursor-pointer rounded-xl">
-                <span class="p-5 rounded-full bg-indigo-400 text-white shadow-lg shadow-black"><svg
-                        xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="1.5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
-                    </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Education</p>
-                <p class="mt-2 text-sm text-slate-500">Yet another year ! Yet another jewel in our crown!</p>
-            </div>
-       </a>
     
         <a href="http://metaverseofthings.blog">
             <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-200 cursor-pointer rounded-xl">
@@ -86,7 +86,7 @@ const About = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                     </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Discussion</p>
+                <p class="text-xl font-medium text-gray-400 mt-3">Discord</p>
                 <p class="mt-2 text-sm text-slate-500">Visa process by helping you create the necessary documentation
                 </p>
             </div>
