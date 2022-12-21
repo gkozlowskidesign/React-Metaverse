66d0071dd478315f9f9c52823419f86d7b290424
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-21 00:32:33 -0500

    Update Discussion.jsx

--- src/componets/Discussion.jsx
+++ src/componets/Discussion.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Social = () => {
 return (
 
-<div class="antialiased bg-black w-full min-h-screen text-slate-300 relative py-4">
+<div class="antialiased bg-black w-full text-slate-300 relative py-4">
     <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
         <div id="menu" class="bg-white/10 col-span-3 rounded-lg p-4">
             <h1 class="mx-auto font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 to-transparent bg-clip-text text-transparent">Connect<span class="text-indigo-400">.</span></h1>
