92cc441122a32b1c1463941757090c3ab107f839
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-06 18:53:29 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -6,7 +6,7 @@ return (
       <div class="grid grid-cols-1 md:lg:xl:grid-cols-3 gap-8 group rounded-xl">
   
   
-              <a href="/documentation">
+              <a href="https://metaverse-of-things.gitbook.io/documentation/">
                       <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
                           <span class="p-5 rounded-full  bg-black text-white shadow-lg shadow-black"><svg
                                   xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
