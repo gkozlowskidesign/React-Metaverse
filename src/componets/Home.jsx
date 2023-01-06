06af05a57457ba40ab9aa7dc28137b1a2c0bb2d6
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-06 17:35:49 -0500

    Update Home.jsx

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 const Home = () => {
 return (
-<div class="bg-black">
-    <div class="bg-black">
+ <div class="bg-gradient-to-b from-black via-purple-900 to-pink-900">
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-            <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                <h2 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white">Future Infrastructure and Applications</h2>
-                <p class="mb-4">We are at a pivtol moment in history where IoT is actually making a difference in both business and personal life operations.
-                We believe the industrial industries which support essential function within our cities is the most forefront for how IoT will play a role in Society.</p>
-                <p>Machines will not replace employees but they will make these essential functions easier through automation, safety, security and connectivity allowing for more consistant optimziations that align with what the business and societal needs. 
-                Development of our base-level IoT software has already begun and we will deliver value for everyone before moving to more centrialized projects.</p><br></br>
-                <p>Our team is made up of strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-            </div>
-            <div class="grid grid-cols-3 gap-4 mt-8">
-            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/2921137/pexels-photo-2921137.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
+            <div class="font-light text-indigo-400">
+                <h2 class="mb-4 text-3xl font-extrabold text-white dark:text-white">Future Infrastructure and Applications</h2>
 
-                <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
-                <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://images.pexels.com/photos/11138000/pexels-photo-11138000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="office content 2"/>
+                <p class="mb-4">We are at a pivtol moment in history where Blockchain, 
+                Cloud-Infrastructure and the Internet and IoT are actually making a difference in 
+                both business and personal life operations.</p>
+               
+                <p  class="mb-4">We aim to build three base services with the intentions of later tokenizing the whole 
+                company and going fully decentralized as a future DAO. Until then we will be focusing on building the brand
+                making three seperate products and services that can actually change the world through IoT and blockchain.</p>
+
+                <p  class="mb-4">The three product/services first focus on creating a base foundation for supporting web3 and helping businesses become smarter, faster, and more autonomous.</p> 
+               
+                <p  class="mb-4">Our team is made up of strategists, designers and developers. Innovators and problem solvers. 
+                Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
             </div>
-   </div>
-</div>
+            <div class="grid grid-cols-1 gap-4 mt-8">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://github.com/gkozlowskidesign/React-MVoT-Website/blob/codespace-gkozlowskidesign-opulent-chainsaw-59jvrpj6p56fp7w5/Intro_Overgrown.png?raw=true" alt="office content 2"/>
+            </div>
+        </div>
+    </div>    
 
                 <div class="mx-auto bg-black mt-40">
         
