c41ce74eb5c159c329365631218ddde2f637b832
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-06 18:06:33 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -19,7 +19,7 @@ const About = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Documentation</p>
+                <p class="text-xl font-medium text-gray-400 mt-3">GitBook</p>
             </div>
       </a>
       <a href="http://metaverseofthings.blog">
@@ -30,20 +30,11 @@ const About = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Announcements</p>
+                <p class="text-xl font-medium text-gray-400 mt-3">Medium</p>
             </div>
       </a>
     
-     <a href="/learning">
-            <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-200 cursor-pointer rounded-xl">
-                <span class="p-5 rounded-full bg-indigo-400 text-white shadow-lg shadow-black"><svg
-                        xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="1.5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
-                    </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Education</p>
-            </div>
-       </a>
+  
     <a href="https://twitter.com/metaverseot">
             <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-200 cursor-pointer rounded-xl">
                 <span class="p-5 rounded-full bg-indigo-400 text-white shadow-lg shadow-black"><svg
@@ -58,29 +49,8 @@ const About = () => {
                 <p class="text-xl font-medium text-gray-400 mt-3">Twitter</p>
             </div>
       </a>
-      <a href="https://www.instagram.com/metaverseofthings/">
-            <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group hover:bg-gray-200 cursor-pointer rounded-xl">
-                <span class="p-5 rounded-full bg-indigo-400 text-white shadow-lg shadow-black"><svg
-                        xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="1.5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                    </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Instagram</p>
-            </div>
-           </a>
+   
     
-        <a href="http://metaverseofthings.blog">
-            <div class="p-10 bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-200 cursor-pointer rounded-xl">
-                <span class="p-5 rounded-full bg-indigo-400 text-white shadow-lg shadow-black"><svg
-                        xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="1.5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                    </svg></span>
-                <p class="text-xl font-medium text-gray-400 mt-3">Discord</p>
-            </div>
-          </a>
         </div>
     </div>
  </div>
