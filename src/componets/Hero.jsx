9462f26fb24fa91ee60bafab1984c678cfbec558
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-06 17:40:32 -0500

    Update Hero.jsx

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import Logo from "../assets/metaverseofthings_logo.png"
 const Hero = () => {
 return (
 <div>
-    <div name="about" className='w-full h-100 bg-gradient-to-b from-indigo-900 to-black'>
-         <div class="w-full h-full  max-w-screen-lg p-6 mx-auto flex flex-col justify-center gap-2 ">
-            <section className='pt-10 py-40 flex items-center h-100 justify-center'>
+    <div name="about" className='w-full h-100 bg-black'>
+         <div class="w-full h-full  max-w-screen-lg p-20 mx-auto flex flex-col justify-center gap-2 ">
+            <section className='py-16 flex items-center h-100 justify-center'>
                 <div className="mx-auto max-w-[27.5rem]">
                 <div className="text-center">
-                <div className="pt-2  flex items-center justify-center ">
-                    <img className="h-30" src={Logo} alt="MVoT"/> 
-                </div>     
-                    <p className="mb-2 text-lg font-medium leading-8 text-white">MVoT Home</p>
-                    <h1 className="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br>
-                    <span className='text-purple-400'>of Things</span></h1>
-                    <p className="mb-2 text-lg font-medium leading-8 text-white">Get Started</p>      
+                <h1 className="mt-20 mb-2 text-[4rem] font-bold leading-[4rem]font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-700 to-pink-300">Metaverse <br></br>
+                <span className='bg-clip-text bg-gradient-to-r from-purple-700 to-pink-300'>of Things</span></h1>
+                <h3 className="mb-2 text-[1rem] font-bold leading-8 text-white">Explore the Destination of Discovery!</h3>      
                 </div>
                 </div>
             </section> 
@@ -23,4 +18,3 @@ return (
 </div>
 )};
 export default Hero
-            
