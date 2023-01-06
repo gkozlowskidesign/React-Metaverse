187dd6de4b552e72a1549178c1b3faaa02da1172
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-06 18:43:17 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -7,34 +7,34 @@ return (
   
   
               <a href="/documentation">
-                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-indigo-500 cursor-pointer rounded-xl">
-                          <span class="p-5 rounded-full bg-pink-300 text-white shadow-lg shadow-black"><svg
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black text-white shadow-lg shadow-black"><svg
                                   xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
                                   stroke="currentColor" stroke-width="1.5">
                                   <path stroke-linecap="round" stroke-linejoin="round"
                                       d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                               </svg></span>
-                          <p class="text-xl font-medium text-indigo-400 mt-3">GitBook</p>
+                          <p class="text-xl font-medium text-gray-500 mt-3">GitBook</p>
                       </div>
                 </a>
   
   
                 <a href="http://metaverseofthings.blog">
-                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group hover:bg-indigo-500 cursor-pointer rounded-xl">
-                          <span class="p-5 rounded-full bg-pink-300 text-white shadow-lg shadow-black"><svg
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full bg-black text-white shadow-lg shadow-black"><svg
                                   xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
                                   stroke="currentColor" stroke-width="1.5">
                                   <path stroke-linecap="round" stroke-linejoin="round"
                                       d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                               </svg></span>
-                          <p class="text-xl font-medium text-indigo-400 mt-3">Medium</p>
+                          <p class="text-xl font-medium text-gray-500 mt-3">Medium</p>
                       </div>
                 </a>
 
 
               <a href="https://twitter.com/metaverseot">
-                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-indigo-500   cursor-pointer rounded-xl">
-                          <span class="p-5 rounded-full bg-pink-300  text-white shadow-lg shadow-black"><svg
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
                                   xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
                                   stroke="currentColor" stroke-width="1.5">
                                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
@@ -43,7 +43,7 @@ return (
                                   <line x1="16" y1="17" x2="8" y2="17"></line>
                                   <polyline points="10 9 9 9 8 9"></polyline>
                               </svg></span>
-                          <p class="text-xl font-medium text-indigo-400 mt-3">Twitter</p>
+                          <p class="text-xl font-medium text-gray-500  mt-3">Twitter</p>
                       </div>
                 </a>
   
