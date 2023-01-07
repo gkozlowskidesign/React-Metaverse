ec16b4de11bca82db214153ac2c6ec8ece9b4e0c
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-07 00:09:06 -0500

    Update Footer.jsx

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Documentation = () => {
 return (
 
-    <div className='w-full px-10 h-100 bg-gradient-to-b from-pink-900 to-black'> 
+    <div className='w-full px-10 h-100 bg-gradient-to-b from-indigo-700 to-black'> 
     <section className='p-10 w-full container mx-auto flex flex-col justify-center gap-10'>
 
     <footer class="relative py-20 flex flex-col items-center overflow-hidden md:py-40">
