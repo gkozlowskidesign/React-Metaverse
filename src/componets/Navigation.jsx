d49a9dfb4ff2809958157f38d5103ad86b406e77
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-08 17:00:50 -0500

    Update Navigation.jsx

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -32,8 +32,8 @@ return (
 			<a class="w-full mx-auto" href="http://svelte.dev/">
 				<img
 					class="w-10 mx-auto"
-					src="https://github.com/gkozlowskidesign/React-MVoT-Website/blob/codespace-gkozlowskidesign-opulent-chainsaw-59jvrpj6p56fp7w5/MVOT%20LOGO%20copy.png?raw=true"
-					alt="svelte logo" />
+					src="https://github.com/gkozlowskidesign/MetaverseOfThings---REACT-Website/blob/Branch_1-7-2023/MVOT%20LOGO%20copy.png?raw=true"
+					alt="MetaverseOfThings Logo" />
 			</a>
 		</div>
 
