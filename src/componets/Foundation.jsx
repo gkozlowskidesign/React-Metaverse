bd993f73de558806fc42eb0a72667d01e2db8139
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 13:41:21 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Foundation = () => {
 return (
   <div>           
-    <div class="px-3 md:lg:xl:px-40 py-20 bg-gradient-to-b from-black to-indigo-700">
+    <div class="px-3 md:lg:xl:px-40 py-20 bg-gradient-to-b from-black via-purple-900 to-indigo-700">
       <div class="grid grid-cols-1 md:lg:xl:grid-cols-3 gap-8 group rounded-xl">
   
   
