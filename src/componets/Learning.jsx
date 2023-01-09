8e934f066168cd43679c4643f53eef4c36400617
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 15:25:52 -0500

    Update Learning.jsx

--- src/componets/Learning.jsx
+++ src/componets/Learning.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Documentation = () => {
 return (
 <div>
-  <div className='w-full h-100 p-2 pb-10 mx-auto bg-white'>
+  <div className='w-full h-100 p-2 pb-10 mx-auto bg-black'>
     <section className='mt-2 flex flex-col justify-center gap-2'>
       <div className="flex flex-wrap">
           <iframe className='mb-2' src="https://mirror.xyz/0x38034A81D48cBf8D4c4C757856d14492C848015b" name="iframe_a" scrolling="no" height="2800rem" width="100%" title="Iframe Example"></iframe>
