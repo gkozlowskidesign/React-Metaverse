ede93810e7a49ba7e1980ff96319d97207b5872c
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 13:57:06 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -3,35 +3,119 @@ const Foundation = () => {
 return (
   <div>           
     <div class="px-3 md:lg:xl:px-40 py-20 bg-gradient-to-b from-black via-purple-900 to-indigo-700">
+        <div class="grid grid-cols-1 md:lg:xl:grid-cols-1 gap-8 group rounded-xl">
+
+   {/* Foundations Linked Object Card */}
+                <a href="http://metaverseot.xyz">
+                      <div class="p-10 w-6/6 mx-auto bg-gray-900 ring-4 ring-purple-600 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
+                                  xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                                  stroke="currentColor" stroke-width="1.5">
+                                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                  <polyline points="14 2 14 8 20 8"></polyline>
+                                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                                  <polyline points="10 9 9 9 8 9"></polyline>
+                              </svg></span>
+                          <p class="text-xl font-medium text-gray-500 mt-3">Featured</p>
+                      </div>
+                </a>
+            </div>
+
       <div class="grid grid-cols-1 md:lg:xl:grid-cols-3 gap-8 group rounded-xl">
-  
-  
+ {/* Foundations Linked Object Card */}
+                <a href="http://metaverseot.xyz">
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 ring-4 ring-purple-600 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
+                                  xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                                  stroke="currentColor" stroke-width="1.5">
+                                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                  <polyline points="14 2 14 8 20 8"></polyline>
+                                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                                  <polyline points="10 9 9 9 8 9"></polyline>
+                              </svg></span>
+                          <p class="text-xl font-medium text-gray-500 mt-3">Mirror</p>
+                      </div>
+                </a>
+ {/* Foundations Linked Object Card */}
+                <a href="http://metaverseot.xyz">
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 ring-4 ring-purple-600 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
+                                  xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                                  stroke="currentColor" stroke-width="1.5">
+                                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                  <polyline points="14 2 14 8 20 8"></polyline>
+                                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                                  <polyline points="10 9 9 9 8 9"></polyline>
+                              </svg></span>
+                          <p class="text-xl font-medium text-gray-500 mt-3">Mirror</p>
+                      </div>
+                </a>
+  {/* Foundations Linked Object Card */}
+              <a href="https://twitter.com/metaverseot">
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
+                                  xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                                  stroke="currentColor" stroke-width="1.5">
+                                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                  <polyline points="14 2 14 8 20 8"></polyline>
+                                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                                  <polyline points="10 9 9 9 8 9"></polyline>
+                              </svg></span>
+                          <p class="text-xl font-medium text-gray-500  mt-3">Medium</p>
+                      </div>
+                </a>
+ {/* Foundations Linked Object Card */}
+              <a href="https://twitter.com/metaverseot">
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
+                                  xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                                  stroke="currentColor" stroke-width="1.5">
+                                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                  <polyline points="14 2 14 8 20 8"></polyline>
+                                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                                  <polyline points="10 9 9 9 8 9"></polyline>
+                              </svg></span>
+                          <p class="text-xl font-medium text-gray-500  mt-3">Twitter</p>
+                      </div>
+                </a>
+  {/* Foundations Linked Object Card */}
               <a href="https://github.com/MetaverseOfThings">
                       <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
-                          <span class="p-5 rounded-full  bg-black text-white shadow-lg shadow-black"><svg
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
                                   xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
                                   stroke="currentColor" stroke-width="1.5">
-                                  <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                  <polyline points="14 2 14 8 20 8"></polyline>
+                                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                                  <polyline points="10 9 9 9 8 9"></polyline>
                               </svg></span>
-                          <p class="text-xl font-medium text-gray-500 mt-3">GitHub</p>
-                      </div>
-                </a>
-  
-  
-                <a href="http://metaverseofthings.blog">
-                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group hover:bg-gray-800 cursor-pointer rounded-xl">
-                          <span class="p-5 rounded-full bg-black text-white shadow-lg shadow-black"><svg
-                                  xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                  stroke="currentColor" stroke-width="1.5">
-                                  <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                              </svg></span>
-                          <p class="text-xl font-medium text-gray-500 mt-3">Medium</p>
+                          <p class="text-xl font-medium text-gray-500  mt-3">GitHub</p>
                       </div>
                 </a>
 
+ {/* Foundations Linked Object Card */}
+              <a href="https://twitter.com/metaverseot">
+                      <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
+                                  xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                                  stroke="currentColor" stroke-width="1.5">
+                                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                  <polyline points="14 2 14 8 20 8"></polyline>
+                                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                                  <polyline points="10 9 9 9 8 9"></polyline>
+                              </svg></span>
+                          <p class="text-xl font-medium text-gray-500  mt-3">Reddit</p>
+                      </div>
+                </a>
 
+ {/* Foundations Linked Object Card */}
               <a href="https://twitter.com/metaverseot">
                       <div class="p-10 w-5/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
                           <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
