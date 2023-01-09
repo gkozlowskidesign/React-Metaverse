9b1df42840897ff011946bcae080b5cd18e964f5
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 14:55:52 -0500

    Update Navigation.jsx

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -85,23 +85,23 @@ return (
 {/* Rocket Navigation Icon */}
 			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
 					<a
-					href="https://mvot-analyitcs.netlify.app/"
+					href="https://metaverseofthings-analyitcs.netlify.app/"
 					class="h-16 px-6 flex flex justify-center items-center w-full
 					focus:text-white">
-					<svg xmlns="http://www.w3.org/2000/svg" 
-					fill="none" 
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" 
 					viewBox="0 0 24 24" 
 					stroke-width="1.5" 
 					stroke="currentColor" 
 					class="w-6 h-6">
-					<path stroke-linecap="round" 
-					stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 
-					0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 
-					0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 
-					15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 
-					004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+ 					<path stroke-linecap="round" 
+					stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 
+					1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 
+					19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0
+					.621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 
+					1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 
+					0 01-1.125-1.125V4.125z" />
 					</svg>
-					</a>
+				</a>
 			</li>
 		</ul>
 </aside>
