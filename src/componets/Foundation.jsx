6f18b08ce414d0dd78b03b0d98f34a46a95c398a
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 14:06:57 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -6,7 +6,7 @@ return (
         <div class="grid grid-cols-1 md:lg:xl:grid-cols-1 gap-8 group rounded-xl">
 
    {/* Foundations Linked Object Card */}
-                <a href="http://metaverseot.xyz">
+                <a href="http://metaverseot.xyz" className="pb-20">
                       <div class="p-10 w-6/6 mx-auto bg-gray-900 ring-4 ring-purple-600 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
                           <span class="p-5 rounded-full  bg-black  text-white shadow-lg shadow-black"><svg
                                   xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
