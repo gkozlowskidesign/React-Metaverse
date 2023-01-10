0574487d8745919f4a9ec2767f8a28594db712a1
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 19:14:34 -0500

    Update Foundation.jsx

--- src/componets/Foundation.jsx
+++ src/componets/Foundation.jsx
@@ -12,8 +12,9 @@ return (
       <div class="grid grid-cols-2 md:lg:xl:grid-cols-4 gap-8 group rounded-xl">
  {/* Foundations Linked Object Card */}
                 <a href="https://gary-kozlowski.gitbook.io/metaverseofthings/">
-                      <div class="p-4 w-6/6 mx-auto bg-gray-900 flex flex-col items-center  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
-                          <span class="p-5 rounded-full  bg-gradient-to-b from-gray-700 via-gray-900 to-gray-700  text-white shadow-lg shadow-black"><svg
+                      <div class="p-4 w-6/6 mx-auto bg-gray-900 flex flex-col items-center  shadow-lg shadow-black  text-center group  hover:bg-gray-800 cursor-pointer rounded-xl">
+                          <span class="p-5 rounded-full  bg-gradient-to-b from-gray-700 via-gray-900 to-gray-700  text-white shadow-lg shadow-black">
+                                  <svg
                                   xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
                                   stroke="currentColor" stroke-width="1.5">
                                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
