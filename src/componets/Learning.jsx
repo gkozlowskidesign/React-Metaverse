bbbcb958552248e8e6d6aa69df34a2498dece696
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 19:11:55 -0500

    Update Learning.jsx

--- src/componets/Learning.jsx
+++ src/componets/Learning.jsx
@@ -5,7 +5,7 @@ return (
   <div className='w-full h-100 pb-10 mx-auto bg-black'>
     <section className='flex flex-col justify-center gap-2'>
       <div className="flex flex-wrap">
-          <iframe className='mb-2' src="" name="iframe_a" scrolling="no" height="2800rem" width="100%" title="Iframe Example"></iframe>
+          <iframe className='mb-2' src="https://mirror.xyz/0x38034A81D48cBf8D4c4C757856d14492C848015b" name="iframe_a" scrolling="no" height="2800rem" width="100%" title="Iframe Example"></iframe>
       </div>
     </section>
    </div>
