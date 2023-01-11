feae56b3518d88fec29a6f03a3475a4eb89ce522
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-10 20:37:34 -0500

    Update Navigation.jsx

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -61,7 +61,7 @@ return (
 					</svg>
 					</a>
 			</li>
-</div> 
+	</div> 
         <div class="bg-green-300 rounded-lg">{/* Dashboard Navigation Icon */}
 			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
 					<a
@@ -84,37 +84,8 @@ return (
 					</svg>
 					</a>
 			</li>
-</div> 
-        <div class="bg-green-500 row-span-2 rounded-lg">4</div> 
-        <div class="bg-green-300 rounded-lg">5</div> 
-        <div class="bg-green-500 row-span-3 rounded-lg">6</div> 
-    </div> 
-		
-			
-
-{/* Dashboard Navigation Icon */}
-			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
-					<a
-					href="/foundation"
-					class="h-16 px-6 flex flex justify-center items-center w-full
-					focus:text-white">
-					<svg xmlns="http://www.w3.org/2000/svg" 
-					fill="none"
-					viewBox="0 0 24 24" 
-					stroke-width="1.5" 
-					stroke="currentColor" 
-					class="w-6 h-6">
-					<path stroke-linecap="round" 
-					stroke-linejoin="round" 
-					d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 
-					2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 
-					0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 
-					2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 
-					2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
-					</svg>
-					</a>
-			</li>
-{/* Learning Navigation Icon */}
+	</div> 
+        <div class="bg-green-500 row-span-2 rounded-lg">{/* Learning Navigation Icon */}
 			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
 					<a
 					href="/learning"
@@ -137,6 +108,8 @@ return (
 					</svg>
 					</a>
 			</li>
+	</div> 
+        <div class="bg-green-300 rounded-lg">
 {/* Chart Navigation Icon */}
 			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
 					<a
@@ -158,7 +131,12 @@ return (
 					</svg>
 				</a>
 			</li>
-		</ul>
+	</div> 
+      </ul>
+    </div> 
+</div>
+		
+
 </aside>
 {/* Navigation Title Text */}
 		<div class="mx-auto flex flex-col">
